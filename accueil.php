cbe1e0452579b517b69069549aaa4185ba3af890
Hurtem <62056819+Hurtem@users.noreply.github.com>
2020-03-27 10:16:34 +0100

    Update and rename accueil.html to accueil.php

---
+++ accueil.php
@@ -0,0 +1,66 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <title>Accueil Société Lafleur</title>
+        <meta charset="UTF-8">
+        <link href="css.css" rel="stylesheet">
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+</head>
+        
+        
+
+    
+    <body class='body'>
+        
+        <header>
+            <h1 class="titre"><img src="Images\ACCUEIL.JPG" alt="Fleur accueil" class="img_header">Société Lafleur</h1>
+            6, cloitre St Aignan
+            <br>
+            45000 Orléans
+        </header>
+        
+
+        <div class="test">
+
+          <div class="menu">
+            
+            <div>
+              <a class="onglets vertical-menu-accueil" href="#">Accueil</a>
+              <a class="nos-produits vertical-menu-produit" href="#">Nos produits</a>
+              <a class="onglets vertical-menu" href="#">Bulbes</a>
+              <a class="onglets vertical-menu" href="#">Plantes à massif</a>
+              <a class="onglets vertical-menu" href="#">Rosiers</a>
+            </div>
+          </div>
+          
+          <div>
+            &nbsp;
+          </div>
+
+          <div class='div-contenu'>
+              "Dites-le avec Lafleur"
+              <br>
+              <br>
+              <img src="Images\ACCUEIL.JPG" alt="Fleur accueil">
+              <br>
+              <br>
+              pour recevoir un bon de commande
+              <br>
+              Appelez notre service commercial au 03.22.84.65.74
+          </div>
+
+      </div>
+
+
+        <footer>
+            <h2 class="pied_page">® copyrights Votre nom réalisé le ...</h2>
+        </footer>
+        
+    </body>
+</html>
