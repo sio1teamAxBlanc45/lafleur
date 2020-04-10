ec7a9bd4a36d87e6931793c3fa20413da6d48169
Hurtem <62056819+Hurtem@users.noreply.github.com>
2020-04-10 11:14:19 +0200

    Update listpdt.php

--- listpdt.php
+++ listpdt.php
@@ -28,47 +28,61 @@
     
     <body class='body'>
         
-        <header>
-            <h1 class="titre"><img src="Images\ACCUEIL.JPG" alt="Fleur accueil" class="img_header">Société Lafleur</h1>
-            6, cloitre St Aignan<br>
-            45000 Orléans
-        </header>
         
 
         <div class="content">
+          <div class="gauche">
+            <div>
+              <header>
+                <h1 class="titre">Sté Lafleur</h1>
+                <hr class="titre">
+                <br>
+                6, cloitre St Aignan
+                <br>
+                45000 Orléans
+              </header>
+            </div>
 
-            <div class="menu">
+            <div class="menu">                    
               <a class="onglets"> 
+                  <form >
+                      <input type="hidden" name="ref" value=""><input type="submit" value="Nous écrire" class="link-lookalike link-lookalike-ecrire">
+                  </form> 
                   <form action="accueil.php">
                       <input type="hidden" name="ref" value="accueil.php"><input type="submit" value="Accueil" class="link-lookalike link-lookalike-acceuil">
                   </form> 
               </a>
 
+              <hr class="new">
+            
+
               <b class="nos-produits" >Nos produits </b>
-              <?php
-              $req = 'SELECT cat_libelle FROM categorie'; 
-              
-              $menu = $connection ->query($req);    
-              
-              while($ligne = $menu->fetch()) { ?>   
-                <a class="onglets">
-                  <form action="tableau.php">
-                      <input type="hidden" name="ref" <?php echo 'value="'.$ligne['cat_libelle'].'"'?>><input type="submit" <?php echo 'value="'.$ligne['cat_libelle'].'"'?> class="link-lookalike ">
-                  </form>
-                </a>
-              <?php 
-              }  
-              ?>
-              
-
-              
+                <?php
+                $req = 'SELECT cat_libelle FROM categorie'; 
+                
+                $menu = $connection ->query($req);    
+                
+                while($ligne = $menu->fetch()) { ?>   
+                  <a class="onglets">
+                    <form action="tableau.php">
+                        <input type="hidden" name="ref" <?php echo 'value="'.$ligne['cat_libelle'].'"'?>><input type="submit" <?php echo 'value="'.$ligne['cat_libelle'].'"'?> class="link-lookalike ">
+                    </form>
+                  </a>
+                <?php 
+                }  
+                ?>
             </div>
-
-            <div>
-            &nbsp;
-            </div>
+            <hr class="new">
+            <form action="panier.php" method="get" class="bouton">
+              <input type="submit" value="Vider le panier">
+            </form>
+            <br>
+            <form action="commande.php" method="get" class="bouton">
+              <input type="submit" value="Commander">
+            </form>
+          </div> 
           
-            <div class='div-contenu'>
+            <div class='div-contenu list'>
                
             <?php
                
@@ -106,12 +120,11 @@
                     </tbody>        
                 </table>                
  
-            
-                
+                             
             </div>   
         </div>
 
-        
+     
 
 
         <footer>
